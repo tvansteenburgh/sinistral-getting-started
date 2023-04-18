c325935970e83f235fe8cd19ddb370fac9f4a104
Dean Henrichsmeyer <dean@beret.net>
2023-04-18 08:54:10 -0500

    Put back sinistral

--- terraform/network/ec2.tf
+++ terraform/network/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "app" {
   ami           = "ami-005e54dee72cc1d00" # us-west-2
   instance_type = "t2.micro"
-  // This resource will fail the Stacklet IaC scan because it has a public IP 
+  // This resource will fail the Sinistral scan because it has a public IP 
   // address and is allowing SSH from anywhere. Fix it by changing the line
   // below to "false".
   associate_public_ip_address = true
